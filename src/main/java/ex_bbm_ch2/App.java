0c838f8d07238ab13ee6e1c28936837fd82c4346
sungwoo <swforgemaster@gmail.com>
2014-02-26 13:57:23 +0900

    Adding resources

---
+++ src/main/java/ex_bbm_ch2/App.java
@@ -0,0 +1,13 @@
+package ex_bbm_ch2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
